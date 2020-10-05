4bbb8eae95eaa84337b410f2fb186a4e34f4fa0f
fiscn <fiscn2709@wrdsb.ca>
2020-10-05 15:07:41 +0000

    Finished A4 Q6 program

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
+import java.util.Scanner; 
+
 /**
  *
- * @author 
+ * @author Neil Fischer
  */
 public class Main {
 
@@ -9,7 +11,31 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    // Create scanner for user input 
+    Scanner input = new Scanner(System.in); 
+
+    //prompt the user to enter in a number 
+    System.out.println("Which number would you like to check?");
+
+    //declare variable for user input and to keep track of division 
+    int user_NM = input.nextInt(); 
+    int Divis = 2;   
+
+    // create while statement to determine if the number is prime or not 
+    while(Divis < user_NM){
+      if(user_NM % Divis == 0){
+        System.out.println(user_NM + " is not a prime number"); 
+      break;
+      }Divis = Divis + 1; 
+    } 
+     if(user_NM == Divis){
+       System.out.println(user_NM + " is a prime number");
+     }
+    
+    
+  
+
+
     
   }
 }
